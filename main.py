cff5559d39a9395808eee3fe74eb3ad1bbc647bc
danijeljw-rpc <danijel@repasscloud.com>
2023-01-02 16:40:00 +1100

    update with working code

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
-import os
-import pypdf2
-import docx
+# Importing the Converter() class
+from pdf2docx import Converter
 
-def pdf_to_docx(pdf_path, docx_path):
-    # Open the PDF file
-    with open(pdf_path, 'rb') as file:
-        # Create a PDF reader object
-        reader = pypdf2.PdfFileReader(file)
+# Specifying the pdf & docx files
+pdf_file = 'orange.pdf'
+docx_file = 'muscles.docx'
 
-        # Create a DOCX file
-        docx = docx.Document()
 
-        # Iterate through each page of the PDF
-        for page in range(reader.getNumPages()):
-            # Extract the text from the page
-            text = reader.getPage(page).extractText()
+try:
+    # Converting PDF to Docx
+    cv_obj = Converter(pdf_file)
+    cv_obj.convert(docx_file)
+    cv_obj.close()
 
-            # Add the text to the DOCX document
-            docx.add_paragraph(text)
-
-        # Save the DOCX file
-        docx.save(docx_path)
-
-# Test the function
-if __name__ == '__main__':
-    pdf_path = 'path/to/input.pdf'
-    docx_path = 'path/to/output.docx'
-    pdf_to_docx(pdf_path, docx_path)
-    print('Successfully converted PDF to DOCX!')
+except:
+    print('Conversion Failed')
+    
+else:
+    print('File Converted Successfully')
