ffd58f5efc819a0c14fb6ab2b493a1fc4a19f8c6
danijeljw-rpc <danijel@repasscloud.com>
2023-01-02 16:29:14 +1100

    terd test

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
-import PyPDF2
+import pypdf2
 import docx
 
 def pdf_to_docx(pdf_path, docx_path):
     # Open the PDF file
     with open(pdf_path, 'rb') as file:
-        # Create a PDF object
-        pdf = PyPDF2.PdfFileReader(file)
+        # Create a PDF reader object
+        reader = pypdf2.PdfFileReader(file)
 
         # Create a DOCX file
         docx = docx.Document()
 
         # Iterate through each page of the PDF
-        for page in range(pdf.getNumPages()):
+        for page in range(reader.getNumPages()):
             # Extract the text from the page
-            text = pdf.getPage(page).extractText()
+            text = reader.getPage(page).extractText()
 
             # Add the text to the DOCX document
             docx.add_paragraph(text)
@@ -24,7 +24,7 @@ def pdf_to_docx(pdf_path, docx_path):
 
 # Test the function
 if __name__ == '__main__':
-    pdf_path = 'engine.pdf'
-    docx_path = 'engine.docx'
+    pdf_path = 'path/to/input.pdf'
+    docx_path = 'path/to/output.docx'
     pdf_to_docx(pdf_path, docx_path)
     print('Successfully converted PDF to DOCX!')
